a689020e16a7baf958bed4d7754d450db33808f4
1C0D <1conscience@gmx.fr>
2024-01-04 15:33:02 +0100

    Updated to version 2.3.4

--- src/constantes.ts
+++ src/constantes.ts
@@ -5,25 +5,16 @@ export const DEFAULT_SETTINGS: CSTSettings = {
     switch: true,
 };
 
-function getEnv(name: string) {
-    let val = process.env[name];
-    return val?.trim() ?? "";
-}
-
-const DEBUG_ACTIVATED = getEnv("DEBUG_ACTIVATED")
-const FORCED_DEBUG_METHOD = getEnv("FORCED_DEBUG_METHOD")
-
-
 export const Console = {
     debug: (...args: any[]) => {
-        if (DEBUG_ACTIVATED) {
-            if (FORCED_DEBUG_METHOD.toLowerCase() === "log") console.log(...args)
+        if ((global as any).DEBUG_ACTIVATED) {
+            if ((global as any).FORCED_DEBUG_METHOD.trim().toLowerCase() === "log") console.log(...args)
             else console.debug(...args);
         }
     },
     log: (...args: any[]) => {
-        if (DEBUG_ACTIVATED) {
-            if (FORCED_DEBUG_METHOD.trim().toLowerCase() === "debug") console.debug(...args)
+        if ((global as any).DEBUG_ACTIVATED) {
+            if ((global as any).FORCED_DEBUG_METHOD.trim().toLowerCase() === "debug") console.debug(...args)
             else console.log(...args);
         }
     }
