a204235897205b7b94a270b4b39990ab8e749de7
1C0D <1conscience@gmx.fr>
2024-01-04 16:28:32 +0100

    Updated to version 2.3.5

--- scripts/start.mjs
+++ scripts/start.mjs
@@ -1,8 +1,8 @@
 import { execSync } from 'child_process';
 
-const currentEvent = process.env.npm_lifecycle_event;
+console.log('- Installed dependencies');
 
-console.log(`Starting the ${currentEvent} process...\n`);
+console.log(`Starting the start process...\n`);
 
 execSync('git fetch');
 console.log('- Fetched git changes');
@@ -10,8 +10,6 @@ console.log('- Fetched git changes');
 execSync('git pull');
 console.log('- Pulled git changes');
 
-execSync('npm install', { stdio: 'inherit' });
-console.log('- Installed dependencies');
 
 execSync("start /B code .", { stdio: "ignore", shell: true });
 console.log('- Opened current folder in VSCode');
