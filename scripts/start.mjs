8d83e3d325a11e86e86c37d2d77687830416647c
1C0D <1conscience@gmx.fr>
2024-01-16 21:27:37 +0100

    dd

---
+++ scripts/start.mjs
@@ -0,0 +1,20 @@
+import { execSync } from 'child_process';
+
+const currentEvent = process.env.npm_lifecycle_event;
+
+console.log(`Starting the ${currentEvent} process...\n`);
+
+execSync('git fetch');
+console.log('- Fetched git changes');
+
+execSync('git pull');
+console.log('- Pulled git changes');
+
+execSync('npm install', { stdio: 'inherit' });
+console.log('- Installed dependencies');
+
+execSync("start /B code .", { stdio: "ignore", shell: true });
+console.log('- Opened current folder in VSCode');
+console.log("- Running 'npm run dev'");
+
+execSync('npm run dev', { stdio: 'inherit' });
